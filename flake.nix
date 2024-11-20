cf699bf461fdb5273d8b467e92301a774b843e60
akuzihs <>
2024-11-20 04:34:40 +0400

    Add ci-translate workflow

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            dos2unix
+            fd
+            git
+            git-lfs
+            go
+            jq
+            moreutils
+          ];
+        };
+      }
+    );
+}
